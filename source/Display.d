1922534fd58600f03672da487e963c92da4b9877
Kxoe <kxoethecat@gmail.com>
2017-10-26 21:19:24 -0600

    renamed Gui to Display, added display code which isn't working.

--- source/Display.d
+++ source/Display.d
@@ -1,5 +1,7 @@
 import gfm.logger;
 import gfm.sdl2;
+import std.experimental.logger;
+import std.stdio;
 /*
 int[2] windowSize = [1000, 800];
 
@@ -15,15 +17,17 @@ enum GuiSlotCoordinates {
 }
 */
 
-class Window{
+class Display{
 
+    Logger logger;
     SDL2 sdl;
     SDL2Window window;
     SDL2Renderer renderer;
-    bool continueRunning;
+    bool quit;
 
     this(int width, int height){
-        this.sdl = new SDL2(null);
+        this.logger = new ConsoleLogger();
+        this.sdl = new SDL2(logger);
         this.window = new SDL2Window(this.sdl, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_SHOWN | SDL_WINDOW_INPUT_FOCUS | SDL_WINDOW_MOUSE_FOCUS);
         this.renderer = new SDL2Renderer(this.window, SDL_RENDERER_SOFTWARE);
         this.window.setTitle("World at War");
@@ -33,6 +37,13 @@ class Window{
         this.sdl.destroy();
         this.window.destroy();
         this.renderer.destroy();
+    }
+
+    void handleEvents(){
+        SDL_Event event;
+        while(this.sdl.pollEvent(&event)){
+            write(event);
+        }
     }
 
 }
--- source/app.d
+++ source/app.d
@@ -1,7 +1,15 @@
 import std.array;
+import gfm.logger;
+import gfm.sdl2;
+import std.experimental.logger;
+import Display;
 
 //TODO: Add documentation to all files
 
 void main(){
-
+	Display display = new Display(1000, 800);
+	while(!display.quit){
+		display.handleEvents();
+	}
+	display.destroy();
 }
