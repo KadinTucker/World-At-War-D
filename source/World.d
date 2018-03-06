9d1ca4246bcabe3c2630eb907622b2674426cec6
Kxoe <kxoethecat@gmail.com>
2018-03-05 21:00:08 -0700

    Added documentation to World and made some modifications to world generation

--- source/World.d
+++ source/World.d
@@ -5,6 +5,9 @@ import City;
 import Player;
 import Unit;
 
+/**
+ * An enumeration of all terrain types
+ */
 enum Terrain {
 
     WATER=0,
@@ -12,6 +15,9 @@ enum Terrain {
 
 }
 
+/**
+ * An enumeration of all directions
+ */
 enum Direction {
     NORTH=0,
     EAST=1,
@@ -19,12 +25,20 @@ enum Direction {
     WEST=3,
 }
 
+/**
+ * A coordinate, with x and y values
+ */
 struct Coordinate {
 
     int x;
     int y;
 
-    Coordinate getCoordinateFromDirection(Direction direction){
+}
+
+/**
+ * Given a coordinate, returns the coordinate that results from moving in the given direction
+ */
+Coordinate moveDirection(Coordinate coord, Direction direction){
         int[2] change;
         if(direction == Direction.NORTH){
             change[1] = -1;
@@ -38,13 +52,13 @@ struct Coordinate {
         if(direction == Direction.WEST){
             change[0] = -1;
         }
-        return Coordinate(x + change[0], y + change[1]);
+        return Coordinate(coord.x + change[0], coord.y + change[1]);
     }
 
-}
-
-
-
+/**
+ * A tile to be found in the world
+ * Has a terrain; may also house a battalion, city, and may be owned by a player as territory
+ */
 class Tile {
 
     Terrain terrain;
@@ -58,41 +72,46 @@ class Tile {
 
 }
 
+/**
+ * The world, mostly as a two-dimensional array of tiles
+ */
 class World {
 
-    Tile[][] tiles = [];
+    Tile[][] tiles; ///The tiles of the world
 
+    /**
+     * Constructs a new world, using the given world parameters
+     * TODO: Add config file to determine worldgen parameters
+     */
     this(int ncontinents, int nrows, int ncols){
-        Tile[] blankArray = [];
-        for(int x = 0; x < nrows; x++){
-            //writeln("iterate outer x",x);
+        Tile[] blankArray;
+        foreach(x; 0..nrows){
             tiles ~= blankArray.dup;
-            //writeln(tiles);
-            for(int y = 0; y < ncols; y++){
+            foreach(y; 0..ncols){
                 tiles[x] ~= new Tile(Terrain.WATER);
             }
         }
-        for(int i = 0; i < ncontinents; i++){
+        foreach(i; 0..ncontinents){
             tiles[uniform(0, nrows - 1)][uniform(0, ncols - 1)].terrain = Terrain.LAND;
         }
         int generated = ncontinents;
         double correlation;
         while (generated < (nrows * ncols) / 3){
-            for(int x = 0; x < tiles.length; x++){
-                for(int y = 0; y < tiles[x].length; y++){
+            foreach(x; 0..tiles.length){
+                foreach(y; 0..tiles[x].length){
                     if(tiles[x][y].terrain != Terrain.LAND){
                         correlation = 0;
                         if(y < tiles[x].length - 1 && tiles[x][y+1].terrain == Terrain.LAND){
-                            correlation += 0.25;
+                            correlation += 0.20;
                         }
                         if(y > 0 && tiles[x][y-1].terrain == Terrain.LAND){
-                            correlation += 0.25;
+                            correlation += 0.20;
                         }
                         if(x < tiles.length - 1 && tiles[x+1][y].terrain == Terrain.LAND){
-                            correlation += 0.25;
+                            correlation += 0.20;
                         }
                         if(x > 0 && tiles[x-1][y].terrain == Terrain.LAND){
-                            correlation += 0.25;
+                            correlation += 0.20;
                         }
                         if(uniform(0.0, 1.0) < correlation){
                             tiles[x][y].terrain = Terrain.LAND;
@@ -104,23 +123,10 @@ class World {
         }
     }
 
+    /**
+     * Returns the tile in the world at the given coordinate
+     */
     Tile getTileAt(Coordinate location){
         return tiles[location.x][location.y];
     }
-}
-
-unittest{
-
-    import std.stdio;
-
-    int number = 89304;
-    writeln(getNthDigit(89304, 5));
-    writeln(getNthDigit(89304, 4));
-    writeln(getNthDigit(89304, 3));
-    writeln(getNthDigit(89304, 2));
-    writeln(getNthDigit(89304, 1));
-
-    World world = new World(4, 30, 40, '_', '+');
-    world.printWorld();
-
 }
